40600da52daf3e96062b3cf706b474fd572fe40d
Mau-Cam98 <goofygarcia23@gmail.com>
2018-03-12 17:41:52 -0600

    se agrega forms a tabla categorias

---
+++ practicafundamentos/categoriasForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practicafundamentos
+{
+    public partial class categoriasForm : Form
+    {
+        public categoriasForm()
+        {
+            InitializeComponent();
+        }
+
+        private void categoriasBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.categoriasBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.postgresDataSet);
+
+        }
+
+        private void categoriasForm_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'postgresDataSet.categorias' Puede moverla o quitarla según sea necesario.
+            this.categoriasTableAdapter.Fill(this.postgresDataSet.categorias);
+
+        }
+    }
+}
